fb81006fff742ced287653e6692eb98c6d78e1ce
jul109 <115905294+jul109@users.noreply.github.com>
2025-10-28 22:34:07 -0500

    feat login: save session

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,18 +1,34 @@
+'use server'
 import { SignInFormSchema, SignInFormState } from '@/app/lib/definitions'
 import { redirect } from 'next/navigation'
+import {createSession} from '@/app/lib/session'
+
 
 export async function signIn(formData: FormData) {
   const validatedFields = SignInFormSchema.safeParse({
     email: formData.get('email'),
     password: formData.get('password'),
   })
- 
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-    }
+  const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL+'/auth/login',
+                               {method: "POST",body: JSON.stringify(validatedFields.data),
+                                 headers: {
+                                   "Content-Type": "application/json",
+                                 }
+  });
+
+  const body =await response.json();
+  if (!body.success){
+    redirect('/login');
   }
-    redirect('/profile')
+  const token= body.token;
+  await createSession(token);
+
+
+  console.log("success");
+
+
+
+
 
 
 }
--- app/lib/session.ts
+++ app/lib/session.ts
@@ -1,37 +1,18 @@
-'server-only'
+
+import 'server-only'
+
 import { SignJWT, jwtVerify } from 'jose';
 import { SessionPayload } from '@/app/lib/definitions';
 import { cookies } from 'next/headers';
  
-const secretKey = process.env.SESSION_SECRET
-const encodedKey = new TextEncoder().encode(secretKey)
- 
-export async function encrypt(payload: SessionPayload) {
-  return new SignJWT(payload)
-    .setProtectedHeader({ alg: 'HS256' })
-    .setIssuedAt()
-    .setExpirationTime('7d')
-    .sign(encodedKey)
-}
- 
-export async function decrypt(session: string | undefined = '') {
-  try {
-    const { payload } = await jwtVerify(session, encodedKey, {
-      algorithms: ['HS256'],
-    })
-    return payload
-  } catch (error) {
-    console.log('Failed to verify session')
-  }
-}
-
-
-export async function createSession(role:string) {
-  const session = await encrypt({role:role})
+export async function createSession(token:any) {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+  const session =   token;
   const cookieStore = await cookies()
+
+ 
   cookieStore.set('session', session, {
     httpOnly: true,
-    secure: true,
     expires: expiresAt,
     sameSite: 'lax',
     path: '/',
